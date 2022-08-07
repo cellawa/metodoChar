810e1c68f1abfe20765f56addd0574b02989019f
louko <w.riosmagalhaes@gmail.com>
2022-08-07 12:39:24 -0300

    MetodoChar

---
+++ src/metodoChar/MetodoChar.java
@@ -0,0 +1,16 @@
+package metodoChar;
+
+public class MetodoChar {
+
+	public static void main(String[] args) {
+/* Char representa uma letra*/
+		char pessoaFeminino = 'f';
+		if(pessoaFeminino == 'f') {
+			System.out.println("Feminino");
+		} else {
+			System.out.println("masculino");
+		}
+		
+	}
+
+}
